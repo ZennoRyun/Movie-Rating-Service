6f401dcbdd6fa21c25313ea4b9178d9d1a6ef152
ZennoRyun <hklee4578@naver.com>
2023-01-03 15:51:16 +0900

    Star Rating Bug Fix

--- src/main/java/com/example/movierating/controller/MovieController.java
+++ src/main/java/com/example/movierating/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.example.movierating.controller;
 import com.example.movierating.api.MovieInfoAPI;
 import com.example.movierating.dto.MovieDTO;
 import com.example.movierating.entity.MovieEntity;
+import com.example.movierating.entity.ReviewEntity;
 import com.example.movierating.service.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,15 +22,18 @@ public class MovieController {
     @GetMapping("/viewMovieInfo")
     public String viewMovieInfo(@RequestParam String movieCd, Model model) throws Exception {
         Optional<MovieEntity> movieEntity = movieService.retrieve(movieCd);
+        ReviewEntity reviewEntity = ReviewEntity.builder().build();
         if(movieEntity.isEmpty()) {
             MovieInfoAPI movieInfoAPI = new MovieInfoAPI();
             MovieDTO dto = movieInfoAPI.getMovieInfo(movieCd);
             MovieEntity entity = MovieDTO.toEntity(dto);
             movieService.createMovie(entity);
             model.addAttribute("movie", entity);
+            model.addAttribute("review", reviewEntity);
         } else {
             movieEntity.get().setRate(Math.round(movieEntity.get().getRate()*100)/100.0);
             model.addAttribute("movie", movieEntity.get());
+            model.addAttribute("review", reviewEntity);
         }
         return "viewMovieInfo";
     }
